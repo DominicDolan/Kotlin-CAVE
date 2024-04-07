7f4b0135a1daf3b82751588667c7c03267b32a4a
Doghouse <dominic.dolan1791@gmail.com>
2024-04-07 02:41:50 +0100

    Added a scale property to InlineVectors

--- src/commonMain/kotlin/com.cave.library/vector/vec2/InlineVector.kt
+++ src/commonMain/kotlin/com.cave.library/vector/vec2/InlineVector.kt
@@ -65,6 +65,8 @@ fun InlineVector.dot(other: InlineVector) = this.x*other.x + this.y*other.y
 
 fun InlineVector.cross(other: InlineVector) = (this.x * other.y - this.y * other.x)
 
+fun InlineVector.scale(other: InlineVector) = vec(this.x * other.x, this.y * other.y)
+
 fun InlineVector.perpendicular() = vec(this.y, -this.x)
 
 val InlineVector.rSquared get() = x*x + y*y
